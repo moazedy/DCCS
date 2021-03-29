baf4720c5870050ef4d0b4d7469b606ec04fee24
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-03-29 17:01:22 -0400

    adding needed constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,9 +1,19 @@
 package constants
 
 const (
-	CouchbaseAddress = "some address" // TODO assigning real database address
+	CouchbaseAddress  = "some address" // TODO assigning real database address
+	CouchbaseUsername = "admin"
+	CouchbasePassword = "adminadmin"
 )
 
 const (
 	ContractsBucket = "contracts"
+)
+
+const (
+	EachTermPercent = 4.7619
+)
+
+const (
+	HttpServingPort = "2121"
 )
