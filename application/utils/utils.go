5952582435bd7a9275360dc3fb4630dfe257e282
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-03-29 11:42:16 -0400

    creating utils package containing needed functions to converting structs to serial and int value

---
+++ application/utils/utils.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"DCCS/domain/models"
+	"reflect"
+	"strconv"
+)
+
+func convertContractToContractSerialDataTemplate(con models.Contract) models.ContractSerialDataTemplate {
+	return models.ContractSerialDataTemplate{
+		GovernmentContract:             con.GovernmentContract,
+		PrivateContract:                con.PrivateContract,
+		Complex:                        con.Complex,
+		StagedExecution:                con.StagedExecution,
+		SpecialLegalRequirements:       con.SpecialLegalRequirements,
+		FixedPricePaymentType:          con.FixedPricePaymentType,
+		CostPlusWagesPaymentType:       con.CostPlusWagesPaymentType,
+		DesignWithContactor:            con.DesignWithContactor,
+		IntegratedContract:             con.IntegratedContract,
+		RiskManagementDone:             con.RiskManagementDone,
+		CounselorManagerNeeded:         con.CounselorManagerNeeded,
+		OwnersMainFactorIsTime:         con.OwnersMainFactorIsTime,
+		OwnersMainFactorInCost:         con.OwnersMainFactorInCost,
+		ContractEvidencesAvailable:     con.ContractEvidencesAvailable,
+		InsuranceOnContractor:          con.InsuranceOnContractor,
+		DesignManagerNeeded:            con.DesignManagerNeeded,
+		SimpleDeliverType:              con.SimpleDeliverType,
+		MaintainingDeliverType:         con.MaintainingDeliverType,
+		DeliverToContractorBeforDesign: con.DeliverToContractorBeforDesign,
+		DeliverToContractorAfterDesign: con.DeliverToContractorAfterDesign,
+		ProcurementOnContractor:        con.ProcurementOnContractor,
+	}
+}
+
+func ToSerialCode(c models.Contract) string {
+	var codedStruct string
+	serialDataTemplate := convertContractToContractSerialDataTemplate(c)
+	values := reflect.ValueOf(serialDataTemplate)
+
+	for i := 0; i < values.NumField(); i++ {
+		if values.Field(i).Interface().(bool) {
+			codedStruct = codedStruct + "1"
+		} else {
+			codedStruct = codedStruct + "0"
+		}
+	}
+
+	return codedStruct
+}
+
+func SerialCodeToIntValue(serial string) (*int64, error) {
+	res, err := strconv.ParseInt(serial, 2, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
